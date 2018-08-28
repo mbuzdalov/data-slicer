87c0c2e24e835bd60284f1224fd7fe69afdb95e5
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-08-28 06:05:47 +0300

    Remove harmful extra quotes.

--- service/src/main/scala/ru/ifmo/ds/srv/Main.scala
+++ service/src/main/scala/ru/ifmo/ds/srv/Main.scala
@@ -92,7 +92,7 @@ object Main {
       val pb = new ProcessBuilder()
       pb.command("sbt",
                  "project benchmarking",
-                 s"jmh:runMain ru.ifmo.nds.jmh.main.Minimal $algorithms --use=$useKey '--out=${outputFile.toAbsolutePath.toString}'")
+                 s"jmh:runMain ru.ifmo.nds.jmh.main.Minimal $algorithms --use=$useKey --out=${outputFile.toRealPath()}")
       pb.inheritIO().directory(root.toFile)
       val exitCode = pb.start().waitFor()
       if (exitCode != 0) {
