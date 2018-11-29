443108a2debddf8c10c5b51b3d81b1ad868ffbb5
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-11-29 18:28:40 +0300

    Fix a bug with multiple `key`s introduced in CompareListener. Once more.

--- service/src/main/scala/ru/ifmo/ds/srv/CompareListener.scala
+++ service/src/main/scala/ru/ifmo/ds/srv/CompareListener.scala
@@ -3,15 +3,15 @@ package ru.ifmo.ds.srv
 import ru.ifmo.ds.ops.FindDifferences.DifferenceListener
 import ru.ifmo.ds.stat.KolmogorovSmirnov
 
-class CompareListener(p: Double, key: String) extends DifferenceListener {
-  private val singleKeySet = Set(key)
+class CompareListener(p: Double, comparedEntityKey: String) extends DifferenceListener {
+  private val singleKeySet = Set(comparedEntityKey)
   private val differingKeys, sameKeys = IndexedSeq.newBuilder[String]
   override def keyValuesDoNotMatch(slice: Map[String, Option[String]], key: String,
                                    onlyLeft: Set[Option[String]],
                                    onlyRight: Set[Option[String]]): Unit = {
     // It can be that the new commit features new keys which did not exist yet. These need to be added.
     // It can be that the new commit deletes some of the old keys. These need not to be added.
-    if (key == this.key) {
+    if (key == comparedEntityKey) {
       onlyRight.foreach(differingKeys ++= _)
     }
   }
@@ -24,7 +24,7 @@ class CompareListener(p: Double, key: String) extends DifferenceListener {
   override def sliceStatistics(slice: Map[String, Option[String]],
                                key: String, statistics: Seq[KolmogorovSmirnov.Result]): Unit = {
     if (slice.keySet == singleKeySet) {
-      slice(key) match {
+      slice(comparedEntityKey) match {
         case None =>
         case Some(value) =>
           val stat = KolmogorovSmirnov.rankSumOnMultipleOutcomes(statistics)
