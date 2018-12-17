1d007185b9bb58d17fa26c769db8543a5a59b1a9
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-12-17 03:38:42 +0300

    Draw tables along the plots, with various UI hacks.

--- gui/src/main/scala/ru/ifmo/ds/gui/Extensions.scala
+++ gui/src/main/scala/ru/ifmo/ds/gui/Extensions.scala
@@ -1,8 +1,13 @@
 package ru.ifmo.ds.gui
 
-import java.awt.{BorderLayout, Color, Paint}
+import java.awt._
+import java.util.{Arrays, Comparator, Locale}
 
 import javax.swing._
+import javax.swing.event.TableModelListener
+import javax.swing.table._
+
+import scala.collection.{mutable => mu}
 
 import org.jfree.chart.labels.XYToolTipGenerator
 import org.jfree.chart.plot.{PlotOrientation, XYPlot}
@@ -63,28 +68,206 @@ object Extensions {
     plot.setDomainGridlinePaint(Color.BLACK)
   }
 
+  private class TableDoubleValueDisplay(val median: Double,
+                                        val min: Double,
+                                        val max: Double,
+                                        alwaysSci: Boolean) extends Comparable[TableDoubleValueDisplay] {
+    private[this] def makeSci(value: Double): String = {
+      val str = "%.03e".formatLocal(Locale.US, value)
+      val eIdx = str.indexOf('e')
+      val rv = if (eIdx < 0) str else {
+        val prefix = str.substring(0, eIdx)
+        val suffix = str.substring(eIdx + 1).toInt
+        prefix + "\u00b710<sup>" + suffix + "</sup>"
+      }
+      rv.replace("-", "&minus;")
+    }
+
+    private[this] def constructValue(value: Double): String = {
+      if (alwaysSci) {
+        makeSci(value)
+      } else {
+        val defVal = value.toString
+        if (defVal.length > 7) makeSci(value) else defVal.replace("-", "&minus;")
+      }
+    }
+
+    def minText: String = constructValue(min)
+    def maxText: String = constructValue(max)
+    def medText: String = constructValue(median)
+
+    override val toString: String = if (median.isNaN) "---" else "<html>" + constructValue(median) + "</html>"
+
+    override def compareTo(o: TableDoubleValueDisplay): Int = {
+      if (median.isNaN) {
+        if (o.median.isNaN) 0 else 1
+      } else if (o.median.isNaN) {
+        -1
+      } else {
+        java.lang.Double.compare(median, o.median)
+      }
+    }
+  }
+
+  private object TableDoubleValueDisplay {
+    final val NaN = new TableDoubleValueDisplay(Double.NaN, Double.NaN, Double.NaN, true)
+    final val CellRenderer = new TableCellRenderer {
+      override def getTableCellRendererComponent(table: JTable, value: Any,
+                                                 isSelected: Boolean, hasFocus: Boolean,
+                                                 row: Int, column: Int): Component = {
+        value match {
+          case v: TableDoubleValueDisplay if !v.min.isNaN && !v.max.isNaN =>
+            val rv = new JLabel(v.toString)
+            rv.setToolTipText(s"<html>median = ${v.medText}<br/>min = ${v.minText}<br/>max = ${v.maxText}</html>")
+            rv
+          case _ => new JLabel(value.toString)
+        }
+      }
+    }
+  }
+
+  private def makeTableRow(data: YIntervalSeries, xValues: Array[Double]): Array[AnyRef] = {
+    val rv = Array.tabulate[AnyRef](xValues.length + 1)(i => if (i == 0) data.getKey.asInstanceOf[AnyRef] else TableDoubleValueDisplay.NaN)
+    for (itemIndex <- 0 until data.getItemCount) {
+      val x = data.getX(itemIndex).doubleValue()
+      //noinspection ReferenceMustBePrefixed
+      val ix = Arrays.binarySearch(xValues, x)
+      assert(ix >= 0)
+      rv(ix + 1) = new TableDoubleValueDisplay(data.getYValue(itemIndex), data.getYLowValue(itemIndex), data.getYHighValue(itemIndex), true)
+    }
+    rv
+  }
+
+  private def prepareTable(data: Seq[YIntervalSeries]): (TableModel, TableRowSorter[TableModel]) = {
+    val xSet = new mu.TreeSet[Double]
+    for (series <- data) {
+      for (itemIndex <- 0 until series.getItemCount) {
+        xSet += series.getX(itemIndex).doubleValue()
+      }
+    }
+    val xValues = xSet.toArray
+    val tableContents = data.map(s => makeTableRow(s, xValues)).toArray
+    val columnNames = "" +: xValues.map(v => new TableDoubleValueDisplay(v, Double.NaN, Double.NaN, false).toString)
+    val tableModel = new TableModel {
+      override def getRowCount: Int = tableContents.length
+      override def getColumnCount: Int = columnNames.length
+      override def getColumnName(columnIndex: Int): String = columnNames(columnIndex)
+      override def getColumnClass(columnIndex: Int): Class[_] = if (columnIndex == 0) classOf[String] else classOf[TableDoubleValueDisplay]
+      override def isCellEditable(rowIndex: Int, columnIndex: Int): Boolean = false
+      override def getValueAt(rowIndex: Int, columnIndex: Int): AnyRef = tableContents(rowIndex)(columnIndex)
+      override def setValueAt(aValue: Any, rowIndex: Int, columnIndex: Int): Unit = throw new UnsupportedOperationException("The table is unmodifiable")
+      override def addTableModelListener(l: TableModelListener): Unit = {}
+      override def removeTableModelListener(l: TableModelListener): Unit = {}
+    }
+    val tableSorter = new TableRowSorter[TableModel](tableModel)
+    for (i <- 1 until columnNames.length) {
+      tableSorter.setComparator(i, Comparator.naturalOrder[TableDoubleValueDisplay]())
+    }
+    (tableModel, tableSorter)
+  }
+
+  private def wrapInControllableSplitter(left: JComponent, leftButtonText: String, right: JComponent, rightButtonText: String): JComponent = {
+    val split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, left, right)
+    split.setDividerLocation(0.5)
+    var lastHorizontalRelativeLocation = 0.5
+    var lastVerticalRelativeLocation = 0.5
+
+    def saveLocation(): Unit = {
+      if (left.isVisible && right.isVisible) {
+        if (split.getOrientation == JSplitPane.HORIZONTAL_SPLIT) {
+          lastHorizontalRelativeLocation = split.getDividerLocation.toDouble / (split.getWidth - split.getDividerSize)
+        } else {
+          lastVerticalRelativeLocation = split.getDividerLocation.toDouble / (split.getHeight - split.getDividerSize)
+        }
+      }
+    }
+
+    val buttonRightOnly = new JButton(rightButtonText)
+    buttonRightOnly.addActionListener(_ => {
+      saveLocation()
+      right.setVisible(true)
+      left.setVisible(false)
+      split.revalidate()
+    })
+    val buttonLeftOnly = new JButton(leftButtonText)
+    buttonLeftOnly.addActionListener(_ => {
+      saveLocation()
+      right.setVisible(false)
+      left.setVisible(true)
+      split.revalidate()
+    })
+    val buttonH = new JButton("|")
+    buttonH.addActionListener(_ => {
+      saveLocation()
+      right.setVisible(true)
+      left.setVisible(true)
+      split.setOrientation(JSplitPane.HORIZONTAL_SPLIT)
+      split.setDividerLocation(lastHorizontalRelativeLocation)
+      split.revalidate()
+    })
+    val buttonV = new JButton("<html>&minus;</html>")
+    buttonV.addActionListener(_ => {
+      saveLocation()
+      right.setVisible(true)
+      left.setVisible(true)
+      split.setOrientation(JSplitPane.VERTICAL_SPLIT)
+      split.setDividerLocation(lastVerticalRelativeLocation)
+      split.revalidate()
+    })
+
+    val buttonPane = new JPanel()
+    val buttonPaneLayout = new GroupLayout(buttonPane)
+    buttonPaneLayout.setAutoCreateGaps(true)
+    buttonPaneLayout.setAutoCreateContainerGaps(true)
+    buttonPane.setLayout(buttonPaneLayout)
+
+    buttonPaneLayout.setVerticalGroup(buttonPaneLayout
+                                        .createSequentialGroup()
+                                        .addGroup(buttonPaneLayout
+                                                    .createParallelGroup()
+                                                    .addComponent(buttonLeftOnly)
+                                                    .addComponent(buttonRightOnly))
+                                        .addGroup(buttonPaneLayout
+                                                    .createParallelGroup()
+                                                    .addComponent(buttonH)
+                                                    .addComponent(buttonV)))
+    buttonPaneLayout.setHorizontalGroup(buttonPaneLayout
+                                          .createSequentialGroup()
+                                          .addGroup(buttonPaneLayout
+                                                      .createParallelGroup()
+                                                      .addComponent(buttonLeftOnly)
+                                                      .addComponent(buttonH))
+                                          .addGroup(buttonPaneLayout
+                                                      .createParallelGroup()
+                                                      .addComponent(buttonRightOnly)
+                                                      .addComponent(buttonV)))
+
+    val rv = new JPanel(new BorderLayout())
+    rv.add(split, BorderLayout.CENTER)
+    rv.add(buttonPane, BorderLayout.LINE_START)
+    rv
+  }
+
   def makeXY(db: Database, categoryKeys: Seq[String], xAxis: Axis, yAxis: Axis, seriesKey: String): JComponent = {
-    type LeafDescription = Seq[YIntervalSeries]
+    case class LeafDescription(plot: XYPlot,
+                               tableModel: TableModel,
+                               tableRowSorter: TableRowSorter[TableModel])
 
     def createJFreeChart(data: LeafDescription): JComponent = {
       assert(SwingUtilities.isEventDispatchThread)
-      val jFreeData = new YIntervalSeriesCollection
-      for (series <- data) {
-        jFreeData.addSeries(series)
-      }
-
-      val plot = new XYPlot(jFreeData, xAxis.toJFreeChartAxis, yAxis.toJFreeChartAxis, new CustomDeviationRenderer)
-      plot.setOrientation(PlotOrientation.VERTICAL)
-      makeUpPlot(plot)
-
-      new ChartPanel(new JFreeChart(plot))
+      val chartPanel = new ChartPanel(new JFreeChart(data.plot))
+      val table = new JTable(data.tableModel)
+      table.setRowSorter(data.tableRowSorter)
+      table.setDefaultRenderer(classOf[TableDoubleValueDisplay], TableDoubleValueDisplay.CellRenderer)
+      val tableScroll = new JScrollPane(table)
+      wrapInControllableSplitter(chartPanel, "C", tableScroll, "T")
     }
 
     def composeSeries(db: Database): LeafDescription = {
       assert(!SwingUtilities.isEventDispatchThread)
       val contents = db.groupMap2D(_.get(seriesKey), _.get(xAxis.key).map(_.toDouble), _.get(yAxis.key).map(_.toDouble))
       val sortedContents = contents.toIndexedSeq.sortBy(_._1)(OrderingForStringWithNumbers.SpecialDotTreatment)
-      sortedContents.map { case (plot, map) =>
+      val data = sortedContents.map { case (plot, map) =>
         val series = new YIntervalSeries(plot)
         for ((x, y) <- map.toIndexedSeq.sortBy(_._1.toInt)) {
           val ySorted = y.toIndexedSeq.sorted
@@ -92,6 +275,18 @@ object Extensions {
         }
         series
       }
+
+      val jFreeData = new YIntervalSeriesCollection
+      for (series <- data) {
+        jFreeData.addSeries(series)
+      }
+
+      val xyPlot = new XYPlot(jFreeData, xAxis.toJFreeChartAxis, yAxis.toJFreeChartAxis, new CustomDeviationRenderer)
+      xyPlot.setOrientation(PlotOrientation.VERTICAL)
+      makeUpPlot(xyPlot)
+
+      val (table, sorter) = prepareTable(data)
+      LeafDescription(xyPlot, table, sorter)
     }
 
     makePresentationTree(db, categoryKeys, composeSeries, createJFreeChart)
