0320ad01ea094741d38017994feec0a8243ef713
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-01-06 02:16:31 +0300

    Make a picture for the create-chart action.

--- gui/src/main/scala/ru/ifmo/ds/gui/PictureGenerator.scala
+++ gui/src/main/scala/ru/ifmo/ds/gui/PictureGenerator.scala
@@ -134,6 +134,15 @@ object PictureGenerator {
     makeArrow(arrow)
   }
 
+  private def makeCreateChart(g: Graphics2D): Unit = {
+    val left = g.create(0, 0, 32, 32).asInstanceOf[Graphics2D]
+    makeDatabase(left)
+    val right = g.create(40, 0, 32, 32).asInstanceOf[Graphics2D]
+    makeChart(right)
+    val arrow = g.create(30, 0, 10, 32).asInstanceOf[Graphics2D]
+    makeArrow(arrow)
+  }
+
   private def write(dir: File, name: String, width: Int, height: Int)(fun: Graphics2D => Any): Unit = {
     val image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR)
     val graphics = image.createGraphics()
@@ -164,5 +173,6 @@ object PictureGenerator {
     write(root, "parts/database.png", 32, 32)(makeDatabase)
 
     write(root, "actions/open-database-files.png", 72, 32)(makeOpenDatabaseFiles)
+    write(root, "actions/create-chart.png", 72, 32)(makeCreateChart)
   }
 }
