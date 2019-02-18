7f14c39f92f58ca94b4869d08e72fd0764b65ee0
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-02-19 01:18:37 +0300

    Refactor the Scaladoc according to the scheme used for case classes.

--- core/src/main/scala/ru/ifmo/ds/stat/TestResult.scala
+++ core/src/main/scala/ru/ifmo/ds/stat/TestResult.scala
@@ -2,32 +2,15 @@ package ru.ifmo.ds.stat
 
 /**
   * This is a class for results computed by statistical tests.
+  *
+  * @param statistic the value of the statistic that was computed on the measurements.
+  * @param p the p-value, that is, the probability that the measurements,
+  *          which are at least as extreme as the given ones according to the value of `statistic`,
+  *          can happen assuming the null hypothesis is true.
+  * @param test the statistical test that generated this result.
+  * @param firstSampleSize the sample size of the first argument.
+  * @param secondSampleSize the sample size of the second argument.
   * @tparam T the type for the statistic.
   */
-case class TestResult[+T](/**
-                            * Returns the value of the statistic that was computed on the measurements.
-                            * @return the value of the statistic.
-                            */
-                          statistic: T,
-                          /**
-                            * Returns the p-value, that is, the probability that the measurements,
-                            * which are at least as extreme as the given ones according to the value of `statistic`,
-                            * can happen assuming the null hypothesis is true.
-                            * @return the p-value.
-                            */
-                          p: Double,
-                          /**
-                            * Returns the statistical test that generated this result.
-                            * @return the statistical test.
-                            */
-                          test: StatisticalTest[T],
-                          /**
-                            * Returns the sample size of the first argument.
-                            * @return the sample size of the first argument.
-                            */
-                          firstSampleSize: Int,
-                          /**
-                            * Returns the sample size of the second argument.
-                            * @return the sample size of the second argument.
-                            */
-                          secondSampleSize: Int)
+case class TestResult[+T](statistic: T, p: Double, test: StatisticalTest[T],
+                          firstSampleSize: Int, secondSampleSize: Int)
