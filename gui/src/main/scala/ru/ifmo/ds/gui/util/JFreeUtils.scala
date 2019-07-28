2a59e637fbbd65a7b0b106f5e0b8cef14ffb892c
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-07-28 15:25:12 +0300

    Drop the badly working tooltips on the plots.

--- gui/src/main/scala/ru/ifmo/ds/gui/util/JFreeUtils.scala
+++ gui/src/main/scala/ru/ifmo/ds/gui/util/JFreeUtils.scala
@@ -2,15 +2,12 @@ package ru.ifmo.ds.gui.util
 
 import java.awt.{Color, Font, Paint}
 
-import scala.Ordering.Double.IeeeOrdering
-
 import org.jfree.chart.axis.{LogarithmicAxis, NumberAxis, ValueAxis}
-import org.jfree.chart.{ChartMouseEvent, ChartMouseListener, ChartPanel}
 import org.jfree.chart.entity.LegendItemEntity
-import org.jfree.chart.labels.XYToolTipGenerator
 import org.jfree.chart.plot.XYPlot
 import org.jfree.chart.renderer.xy.DeviationRenderer
-import org.jfree.data.xy.{IntervalXYDataset, XYDataset}
+import org.jfree.chart.{ChartMouseEvent, ChartMouseListener, ChartPanel}
+import org.jfree.data.xy.XYDataset
 
 import ru.ifmo.ds.util.Axis
 
@@ -25,10 +22,7 @@ object JFreeUtils {
 
   class CustomDeviationRenderer extends DeviationRenderer {
     setAlpha(0.5f)
-    setDefaultToolTipGenerator(JFreeUtils.extendedTooltipGenerator)
-    override def lookupSeriesFillPaint(series: Int): Paint = {
-      super.lookupSeriesPaint(series)
-    }
+    override def lookupSeriesFillPaint(series: Int): Paint = super.lookupSeriesPaint(series)
   }
 
   def initializePlotColors(plot: XYPlot): Unit = {
@@ -43,43 +37,6 @@ object JFreeUtils {
     val legend = plot.getLegendItems
     plot.setFixedLegendItems(legend)
     panel.addChartMouseListener(toggleListener)
-  }
-
-  val extendedTooltipGenerator: XYToolTipGenerator = new XYToolTipGenerator {
-    private def generateToolTips(dataset: XYDataset, series: Int, item: Int): Array[(Double, String)] = {
-      (0 until dataset.getSeriesCount).flatMap { s =>
-        if (dataset.getItemCount(s) > item) {
-          val x = dataset.getX(s, item).intValue()
-          val y = dataset.getYValue(s, item)
-          val prefix = if (s == series) "<b>" else ""
-          val suffix = if (s == series) "</b>" else ""
-          Some(y -> f"$prefix${dataset.getSeriesKey(s)}: $x => $y%.3e$suffix")
-        } else None
-      }.toArray
-    }
-
-    private def generateIntervalToolTips(dataset: IntervalXYDataset, series: Int, item: Int): Array[(Double, String)] = {
-      (0 until dataset.getSeriesCount).flatMap { s =>
-        if (dataset.getItemCount(s) > item) {
-          val x = dataset.getX(s, item).intValue()
-          val y = dataset.getYValue(s, item)
-          val yMin = dataset.getStartYValue(s, item)
-          val yMax = dataset.getEndYValue(s, item)
-          val prefix = if (s == series) "<b>" else ""
-          val suffix = if (s == series) "</b>" else ""
-          Some(y -> f"$prefix${dataset.getSeriesKey(s)}: $x => [$yMin%.3e; $y%.3e, $yMax%.3e]$suffix")
-        } else None
-      }.toArray
-    }
-
-    override def generateToolTip(dataset: XYDataset, series: Int, item: Int): String = {
-      val allRows = dataset match {
-        case d: IntervalXYDataset => generateIntervalToolTips(d, series, item)
-        case d => generateToolTips(d, series, item)
-      }
-      val sortedRows = allRows.sortBy(-_._1).map(_._2) // tooltip rows top-down
-      sortedRows.mkString("<html>", "<br/>", "</html>")
-    }
   }
 
   private val toggleListener = new ChartMouseListener {
