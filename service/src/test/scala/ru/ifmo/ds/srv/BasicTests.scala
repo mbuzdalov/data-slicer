6de5d06c9dd2c7f5f53fc7f9bdd0cee44b90291c
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-12-18 19:09:48 +0300

    Migrate also the service tests.

--- service/src/test/scala/ru/ifmo/ds/srv/BasicTests.scala
+++ service/src/test/scala/ru/ifmo/ds/srv/BasicTests.scala
@@ -1,8 +1,9 @@
 package ru.ifmo.ds.srv
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class BasicTests extends FlatSpec with Matchers {
+class BasicTests extends AnyFlatSpec with Matchers {
   "A call to Main with no arguments" should "fail" in {
     assertThrows[RuntimeException] {
       Main.main(new Array[String](0))
