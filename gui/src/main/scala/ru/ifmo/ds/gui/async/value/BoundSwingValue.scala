2f36b272ee166946ce9be7f91610401145df71b6
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-12-27 02:37:55 +0300

    Make SwingValue descendants covariant in return type.
    BoundSwingValue is rewritten because the old implementation
    made IDEA think that there is a type error, while the Scala
    compiler does not think so. The new implementation isolates
    the workload, so that it is easier for IDEA to find out
    there is nothing wrong.

--- gui/src/main/scala/ru/ifmo/ds/gui/async/value/BoundSwingValue.scala
+++ gui/src/main/scala/ru/ifmo/ds/gui/async/value/BoundSwingValue.scala
@@ -3,11 +3,25 @@ import javax.swing.SwingUtilities
 
 import ru.ifmo.ds.gui.async.node.{MutableNode, Node, Workload}
 
-class BoundSwingValue[I, R](packer: => I, executor: I => R, dependencies: Node*) extends SwingValue[R] {
+class BoundSwingValue[I, +R](packer: => I, executor: I => R, dependencies: Node*) extends SwingValue[R] {
   require(SwingUtilities.isEventDispatchThread)
 
-  private[this] var theValue: Option[R] = None
-  private[this] val workload: Workload = new Workload {
+  private[this] val workload = new BoundSwingValue.PackerExecutorWorkload(packer, executor)
+  private[this] val theNode = new MutableNode(workload)
+
+  dependencies.foreach(_.addListener(theNode))
+  theNode.completeInitialization()
+
+  override def value: R = workload.value
+  override protected[value] def node: Node = theNode
+  override def close(): Unit = dependencies.foreach(_.removeListener(theNode))
+}
+
+object BoundSwingValue {
+  private class PackerExecutorWorkload[I, R](packer: => I, executor: I => R) extends Workload {
+    private[this] var theValue: Option[R] = None
+    def value: R = theValue.get
+
     override type MainInput = I
     override type MainOutput = R
     override def onError(th: Throwable): Unit = theValue = None
@@ -18,12 +32,4 @@ class BoundSwingValue[I, R](packer: => I, executor: I => R, dependencies: Node*)
       packer
     }
   }
-  private[this] val theNode = new MutableNode(workload)
-
-  dependencies.foreach(_.addListener(theNode))
-  theNode.completeInitialization()
-
-  override def value: R = theValue.get
-  override protected[value] def node: Node = theNode
-  override def close(): Unit = dependencies.foreach(_.removeListener(theNode))
 }
--- gui/src/main/scala/ru/ifmo/ds/gui/async/value/ConstantSwingValue.scala
+++ gui/src/main/scala/ru/ifmo/ds/gui/async/value/ConstantSwingValue.scala
@@ -2,7 +2,7 @@ package ru.ifmo.ds.gui.async.value
 
 import ru.ifmo.ds.gui.async.node.Node
 
-class ConstantSwingValue[T](val value: T) extends SwingValue[T] {
+class ConstantSwingValue[+T](val value: T) extends SwingValue[T] {
   override protected[value] def node: Node = Node.DoneNode
   override def close(): Unit = {}
 }
