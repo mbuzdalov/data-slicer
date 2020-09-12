0c610ef6229aba9fa595f2ba9b75402163ffb6d3
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-09-12 04:41:19 +0300

    Shorten a few variable names in LiftingTests.

--- swing-async/src/test/scala/com/github/mbuzdalov/swingasync/ng/LiftingTests.scala
+++ swing-async/src/test/scala/com/github/mbuzdalov/swingasync/ng/LiftingTests.scala
@@ -9,18 +9,18 @@ class LiftingTests extends CommonTesting {
 
   "liftInSwing" should "compile and pass basic checks" in {
     val add = (a: Int, b: Int) => a + b
-    val liftedFunction = Lifting.liftInSwing(add)
+    val lfAdd = Lifting.liftInSwing(add)
 
-    checkSame(done(5), liftedFunction(done(2), done(3)))
-    checkSame(waiting, liftedFunction(done(2), waiting))
-    checkSame(waiting, liftedFunction(waiting, done(3)))
-    checkSame(waiting, liftedFunction(waiting, waiting))
+    checkSame(done(5), lfAdd(done(2), done(3)))
+    checkSame(waiting, lfAdd(done(2), waiting))
+    checkSame(waiting, lfAdd(waiting, done(3)))
+    checkSame(waiting, lfAdd(waiting, waiting))
 
-    val testException = new ArithmeticException("Test exception")
-    checkSame(failed(new DependenciesFailedException(Some(testException))), liftedFunction(done(2), failed(testException)))
-    checkSame(failed(new DependenciesFailedException(Some(testException))), liftedFunction(failed(testException), done(3)))
-    checkSame(failed(new DependenciesFailedException(Some(testException))), liftedFunction(waiting, failed(testException)))
-    checkSame(failed(new DependenciesFailedException(Some(testException))), liftedFunction(failed(testException), waiting))
-    checkSame(failed(new DependenciesFailedException(Seq(testException, testException))), liftedFunction(failed(testException), failed(testException)))
+    val testEx = new ArithmeticException("Test exception")
+    checkSame(failed(new DependenciesFailedException(Some(testEx))), lfAdd(done(2), failed(testEx)))
+    checkSame(failed(new DependenciesFailedException(Some(testEx))), lfAdd(failed(testEx), done(3)))
+    checkSame(failed(new DependenciesFailedException(Some(testEx))), lfAdd(waiting, failed(testEx)))
+    checkSame(failed(new DependenciesFailedException(Some(testEx))), lfAdd(failed(testEx), waiting))
+    checkSame(failed(new DependenciesFailedException(Seq(testEx, testEx))), lfAdd(failed(testEx), failed(testEx)))
   }
 }
