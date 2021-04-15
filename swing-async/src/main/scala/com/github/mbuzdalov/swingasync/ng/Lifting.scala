cf54977639d97dbb64584e8f1772b8328375239b
Maxim Buzdalov <mbuzdalov@gmail.com>
2021-04-16 02:48:19 +0300

    Add a lifting function that maps Option's None to Fail.

--- swing-async/src/main/scala/com/github/mbuzdalov/swingasync/ng/Lifting.scala
+++ swing-async/src/main/scala/com/github/mbuzdalov/swingasync/ng/Lifting.scala
@@ -15,4 +15,37 @@ object Lifting {
     def internalLift(wArgs: LG) = new InSwingFunctionalValue(wArgs, extractor, unwrappedFunction)
     gen2fun(internalLift)
   }
+
+  def liftNoneToFail[A, B](function: A => Option[B], noneMessage: String): UpdatableValue[A] => UpdatableValue[B] = {
+    a => new UpdatableValue[B] {
+      private[this] var myState: UpdatableValue.State[B] = UpdatableValue.Waiting
+      private def setNewState(newState: UpdatableValue.State[B]): Unit = {
+        val oldState = myState
+        myState = newState
+        notifyListeners(oldState)
+      }
+      private val myListener = new UpdatableValue.Listener[A] {
+        private def digestOtherState(state: UpdatableValue.State[A]): Unit = setNewState(state match {
+          case UpdatableValue.Done(v) => function(v) match {
+            case Some(w) => UpdatableValue.Done(w)
+            case None => UpdatableValue.Failed(new NoneToFailException(noneMessage))
+          }
+          case s@UpdatableValue.Failed(_) => s
+          case UpdatableValue.Initializing => UpdatableValue.Initializing
+          case UpdatableValue.Waiting => UpdatableValue.Waiting
+          case UpdatableValue.Running => UpdatableValue.Running
+          case UpdatableValue.Restarting => UpdatableValue.Restarting
+        })
+
+        override def addedToValue(value: UpdatableValue[A]): Unit = digestOtherState(value.state)
+        override def valueChanged(value: UpdatableValue[A], oldState: UpdatableValue.State[A]): Unit = digestOtherState(value.state)
+        override def removedFromValue(value: UpdatableValue[A]): Unit = setNewState(UpdatableValue.Failed(new NoneToFailException(noneMessage)))
+      }
+      a.addListener(myListener)
+
+      override def state: UpdatableValue.State[B] = myState
+    }
+  }
+
+  private class NoneToFailException(message: String) extends IllegalStateException(message)
 }
